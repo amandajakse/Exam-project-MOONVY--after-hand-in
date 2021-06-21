493de4a110ae424ceb51625439c6ed7a17acc95d
KarolinaKrysiak <71433414+KarolinaKrysiak@users.noreply.github.com>
2021-06-21 13:55:56 +0200

    pop up window

--- MOONVY_code/js/js.js
+++ MOONVY_code/js/js.js
@@ -148,29 +148,43 @@ function myFunction6() {
 
 
 
-/** event listener for 'send message' button **/
+/** pop up window after using contact form - https://www.w3schools.com/howto/howto_css_modals.asp **/
 
-try {
-	document.getElementById("sendmssg").addEventListener("click", myFunction)
-} catch {
-	console.log('id sendmssg not found')
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("window");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// Get the button that closes the modal
+var btnClose = document.getElementsByClassName("close_button")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+	modal.style.display = "block";
 }
 
-function myFunction() {
-	alert("Your message has been sent. MOONVY will get back at you shortly.");
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+	modal.style.display = "none";
+}
+
+// When the user clicks on button, close the modal
+btnClose.onclick = function () {
+	modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+	if (event.target == modal) {
+		modal.style.display = "none";
+	}
 }
 
 
-/** event listener for 'add to bag' button **/
-try {
-	document.getElementById("addBtn").addEventListener("click", addToBag)
-} catch {
-	console.log('id addBtn not found')
-}
-
-function addToBag() {
-	alert("Item added to bag");
-}
 
 
 
